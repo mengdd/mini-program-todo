07ebb8844d75ecea5922a8bc18243700d09e3a14
Dandan Meng <mengdandanno1@163.com>
2020-04-20 14:45:21 +0800

    show a static list

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,32 +3,34 @@ import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
     navigationBarTitleText: '首页'
   }
 
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: ['get up', 'eat breakfast', 'study',],
+      inputVal: ''
+    }
+
+  }
+
+  render() {
+    let { list, inputVal } = this.state
+
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <View className='list_wrap'>
+          <Text>Todo List</Text>
+          {
+            list.map((item, index) => {
+              return <View>
+                <Text>{index + 1}.{item}</Text>
+              </View>
+            })
+          }
+        </View>
       </View>
     )
   }
